1628b185250fdf503305bab6d0f32783237cd2b0
Derrick Hargwood <optimus@OptimusPrime.(none)>
2014-04-15 15:59:42 -0400

    Can listen to more than one topic

--- rosComm/listener.py
+++ rosComm/listener.py
@@ -12,29 +12,61 @@ class listen():
 	def __init__(self):
 		print 'init'
 		self.IMUlisten()
-		#self.GPSlisten()
-	
+		self.GPSlisten()
+		self.head = 99
+		self.left = 1
+		self.center = 2
+		self.right = 3
+		self.lat = 147
+		self.lon = 109	
+		self.twist = 0.001
+		self.numSat = 0
+		
 	# Callback function for IMUlisten
 	def IMUinfo(self,data):
 		self.accel = data.linear_acceleration
-		self.gryo = data.angular_velocity
-		self.magne = data.orientation		
+		self.gyro = data.angular_velocity
+		self.magne = data.orientation
+		
 					
 	# ROS subscriber to get IMU data
 	def IMUlisten(self):
-		rospy.init_node('IMUlisten', anonymous = True)
+		#rospy.init_node('IMUlisten', anonymous = True)
 		rospy.Subscriber('IMU',Imu,self.IMUinfo)
-		print 'no-spin'
-'''
+
 	# Callback function for GPSlisten
 	def GPSinfo(self,data):
 		self.lat = data.latitude
 		self.lon = data.longitude
-		self.alt = data.altitude	
+		self.alt = data.altitude
+		pok = data.status
+		self.numSat = pok.service	
 
 	# ROS subscriber to get GPS data
 	def GPSlisten(self):
 		rospy.init_node('GPSlisten', anonymous = True)
 		rospy.Subscriber('GPS',NavSatFix,self.GPSinfo)
+		
+"""
+	# Callback function for HEADlisten
+	def HEADinfo(self,data):
+		self.head = data
+
+	# ROS subscriber to get heading data
+	def HEADlisten(self):
+		rospy.init_node('HEADlisten', anonymous = True)
+		rospy.Subscriber('GPS/Heading',float,self.HEADinfo)
 		rospy.spin()
-'''
+
+	def JPGinfo(self,data)
+		#Need to check data type
+		self.img = data.data
+
+	#ROS subscriber to get Images
+	def JPGlisten(self)
+		rospy.init_node('JPGlisten', anonymous = True)
+		#Need to add correct topic name and msg type
+		rospy.subscriber('/cameras/...',MSGTYPE,self.JPGinfo)
+		#Also, spin might not be necessary more than once
+		rospy.spin()
+"""
